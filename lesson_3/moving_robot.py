f7f8f4e7715dd3b46e35682e896bc578f057ac6f
Zclarkwilliams <zclarkw112@gmail.com>
2018-09-04 14:00:18 -0700

    added robo moving code

--- lesson_3/moving_robot.py
+++ lesson_3/moving_robot.py
@@ -26,17 +26,17 @@ class robot:
         self.x = random.random() * world_size
         self.y = random.random() * world_size
         self.orientation = random.random() * 2.0 * pi
-        self.forward_noise = 0.0;
-        self.turn_noise    = 0.0;
-        self.sense_noise   = 0.0;
+        self.forward_noise = 0.0
+        self.turn_noise    = 0.0
+        self.sense_noise   = 0.0
     
     def set(self, new_x, new_y, new_orientation):
         if new_x < 0 or new_x >= world_size:
-            raise ValueError, 'X coordinate out of bound'
+            raise ValueError('X coordinate out of bound')
         if new_y < 0 or new_y >= world_size:
-            raise ValueError, 'Y coordinate out of bound'
+            raise ValueError('Y coordinate out of bound')
         if new_orientation < 0 or new_orientation >= 2 * pi:
-            raise ValueError, 'Orientation must be in [0..2pi]'
+            raise ValueError('Orientation must be in [0..2pi]')
         self.x = float(new_x)
         self.y = float(new_y)
         self.orientation = float(new_orientation)
@@ -61,7 +61,7 @@ class robot:
     
     def move(self, turn, forward):
         if forward < 0:
-            raise ValueError, 'Robot cant move backwards'         
+            raise ValueError('Robot cant move backwards')         
         
         # turn, and add randomness to the turning command
         orientation = self.orientation + float(turn) + random.gauss(0.0, self.turn_noise)
@@ -90,7 +90,7 @@ class robot:
         
         # calculates how likely a measurement should be
         
-        prob = 1.0;
+        prob = 1.0
         for i in range(len(landmarks)):
             dist = sqrt((self.x - landmarks[i][0]) ** 2 + (self.y - landmarks[i][1]) ** 2)
             prob *= self.Gaussian(dist, self.sense_noise, measurement[i])
@@ -99,21 +99,42 @@ class robot:
     
     
     def __repr__(self):
-        return '[x=%.6s y=%.6s orient=%.6s]' % (str(self.x), str(self.y), str(self.orientation))
+        return ('[x=%.6s y=%.6s orient=%.6s]' % (str(self.x), str(self.y), str(self.orientation)))
 
 
 
-def eval(r, p):
-    sum = 0.0;
+def evaluate(r, p):
+    sum_fin = 0.0;
     for i in range(len(p)): # calculate mean error
         dx = (p[i].x - r.x + (world_size/2.0)) % world_size - (world_size/2.0)
         dy = (p[i].y - r.y + (world_size/2.0)) % world_size - (world_size/2.0)
         err = sqrt(dx * dx + dy * dy)
-        sum += err
-    return sum / float(len(p))
+        sum_fin += err
+    return sum_fin / float(len(p))
 
 
 
 ####   DON'T MODIFY ANYTHING ABOVE HERE! ENTER CODE BELOW ####
 
+# starts at 30.0, 50.0, heading north (=pi/2)
+# turns clockwise by pi/2, moves 15m
+# senses
+# turns clockwise by pi/2, moves 10m
+# senses
+
+## My Code Here ##
 myrobot = robot()
+myrobot.set(30.0, 50.0, pi/2)
+print(myrobot)
+myrobot = myrobot.move(-pi/2, 15)
+print(myrobot)
+print(myrobot.sense())
+myrobot = myrobot.move(-pi/2, 10)
+print(myrobot)
+print(myrobot.sense())
+
+'''
+output to match:
+[39.0512, 46.0978, 39.051, 46.0978]
+[32.0156, 53.1507, 47.169, 40.3113]
+'''
