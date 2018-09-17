a1b516afe467526d41f39611623d41161dbae6a3
Zachary Clark-Williams <zclarkw112@gmail.com>
2018-09-17 13:37:23 -0700

    Update first_search.py

--- lesson_4/first_search.py
+++ lesson_4/first_search.py
@@ -1,1 +1,48 @@
 #! python 3
+# ----------
+# User Instructions:
+# 
+# Define a function, search() that returns a list
+# in the form of [optimal path length, row, col]. For
+# the grid shown below, your function should output
+# [11, 4, 5].
+#
+# If there is no valid path from the start point
+# to the goal, your function should return the string
+# 'fail'
+# ----------
+
+# Grid format:
+#   0 = Navigable space
+#   1 = Occupied space
+
+
+grid = [[0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0],
+        [0, 0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 1, 0]]
+init = [0, 0]
+goal = [len(grid)-1, len(grid[0])-1]
+cost = 1
+
+delta = [[-1, 0], # go up
+         [ 0,-1], # go left
+         [ 1, 0], # go down
+         [ 0, 1]] # go right
+
+delta_name = ['^', '<', 'v', '>']
+
+def search(grid,init,goal,cost):
+    # ----------------------------------------
+    # insert code here
+    # ----------------------------------------
+    #get initial starting point and make that open set point 0
+    open_set = []
+    open_set = init
+    
+    # Test each direction that can be taken to 
+
+    
+    
+    return path
